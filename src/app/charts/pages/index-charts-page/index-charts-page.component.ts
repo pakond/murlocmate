d1dcf18e6c166abc687f3e8dca06bb3671aade99
Pau Albert Ganduil <palga85@gmail.com>
2021-06-02 11:40:43 +0200

    commit

--- src/app/charts/pages/index-charts-page/index-charts-page.component.ts
+++ src/app/charts/pages/index-charts-page/index-charts-page.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { LeaderResult } from 'src/app/leaderboards/interfaces/leaderboards.interfaces';
+import { ChartsService } from '../../services/charts.service';
 
 @Component({
   selector: 'app-index-charts-page',
-  templateUrl: './index-charts-page.component.html'
+  templateUrl: './index-charts-page.component.html',
+  styleUrls: ['./index-charts-page.component.scss']
 })
 export class IndexChartsPageComponent implements OnInit {
 
-  constructor() { }
+  charts!: LeaderResult[];
+  bracket: string = '';
+
+  constructor(
+    private chartsService: ChartsService
+  ) { }
 
   ngOnInit(): void {
+    if (this.bracket !== '2v2' && this.bracket !== '3v3' && this.bracket !== 'rbg') { this.bracket = '3v3' }
+
+    this.chartsService.getCharts(this.bracket)
+      .subscribe(resp => {
+        
+      });
   }
 
+  changeBracket(value: string) {
+    this.bracket = value;
+    this.chartsService.getCharts(this.bracket)
+      .subscribe(resp => {
+        
+      });
+  }
 }
--- src/app/charts/services/charts.service.ts
+++ src/app/charts/services/charts.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LeaderResult } from 'src/app/leaderboards/interfaces/leaderboards.interfaces';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCharts(bracket: string) {
+
+    let url: string = '';
+    if (bracket === '2v2') {
+      url = `${environment.apiUrl}/charts-2v2/`;
+    } else if (bracket === '3v3') {
+      url = `${environment.apiUrl}/charts-3v3/`;
+    } else if (bracket === 'rbg') {
+      url = `${environment.apiUrl}/charts-rbg/`;
+    }
+
+    const headers = new HttpHeaders().set('X-Api-Key', environment.apiKey)
+
+    return this.http.get<LeaderResult[]>(url, { headers });
+  }
 }
